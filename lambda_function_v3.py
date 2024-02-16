18b59999a0a555b88e04bac7d272e96fca8d7cc9
arpeggito <andresbocarp1@gmail.com>
2024-02-16 01:00:28 +0100

    update database in python script

--- lambda_function_v3.py
+++ lambda_function_v3.py
@@ -21,6 +21,21 @@ def transform_payload(payload):
     }
     return transformed_payload
 
+def save_to_s3(payload):
+    try:
+        # Create folder path based on event type and subtype
+        folder_path = f"prefix/{payload['event_type']}/{payload['event_subtype']}/"
+
+        # Save transformed payload to S3 bucket
+        s3_client.put_object(
+            Bucket="babbel-challenge-v1",
+            Key=f"{folder_path}{payload['event_uuid']}.json",
+            Body=json.dumps(payload),
+        )
+        logger.info("Payload saved to S3")
+    except Exception as e:
+        logger.error(f"Error saving payload to S3: {e}")
+
 def lambda_handler(event, context):
     for record in event["Records"]:
         try:
@@ -35,17 +50,4 @@ def lambda_handler(event, context):
         except Exception as e:
             logger.error(f"Error processing event: {e}")
 
-def save_to_s3(payload):
-    try:
-        # Create folder path based on event type and subtype
-        folder_path = f"prefix/{payload['event_type']}/{payload['event_subtype']}/"
 
-        # Save transformed payload to S3 bucket
-        s3_client.put_object(
-            Bucket="Babbel-Challenge",
-            Key=f"{folder_path}{payload['event_uuid']}.json",
-            Body=json.dumps(payload),
-        )
-        logger.info("Payload saved to S3")
-    except Exception as e:
-        logger.error(f"Error saving payload to S3: {e}")
